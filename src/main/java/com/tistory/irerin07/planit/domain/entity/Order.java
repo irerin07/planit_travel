8054b701842ba3770acc3bf3c19efc98d16bf85f
irerin07 <counterchord89@gmail.com>
2023-09-25 14:50:10 +0900

    Initial commit

---
+++ src/main/java/com/tistory/irerin07/planit/domain/entity/Order.java
@@ -0,0 +1,9 @@
+package com.tistory.irerin07.planit.domain.entity;
+
+/**
+ * @author 민경수
+ * @description order
+ * @since 2023.09.25
+ **********************************************************************************************************************/
+public class Order {
+}
---
+++ src/main/java/com/tistory/irerin07/planit/domain/entity/Plan.java
@@ -0,0 +1,9 @@
+package com.tistory.irerin07.planit.domain.entity;
+
+/**
+ * @author 민경수
+ * @description plan
+ * @since 2023.09.25
+ **********************************************************************************************************************/
+public class Plan {
+}
